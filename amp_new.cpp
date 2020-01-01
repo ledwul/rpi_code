800fd4e0c8213cd6a4e6228366872dbbb8c31911
Raspberry Rocketman <blub>
2020-01-01 16:40:21 +0000

    Add commentary

--- amp_new.cpp
+++ amp_new.cpp
@@ -1,5 +1,4 @@
 #include "amp_new.h"
-#include <current_control.h>
 
 using namespace std;
 
@@ -7,49 +6,16 @@ const int I2C_ADDR = 0x48;
 const float SHUNT_OHMS = 0.50;
 short val[4];
 
-float getTarget(int light, float time){
-  //if (light != 0) return 0;
-  //return 0.3 * (0.5 + 0.5* (sin(2*M_PI*time/50 + 0.25*(light%2) * M_PI)));
-  const float target_brightness = 0.3;
-  if (time > 0) return target_brightness;
-  float ramp_time ;
-  if (light % 2 == 0){
-    ramp_time = 10;
-  }else{
-    ramp_time = 20;
-  }
-  if (time < -ramp_time) return 0;
-  return target_brightness * (ramp_time + time) / ramp_time;
-}
-
-std::chrono::high_resolution_clock::time_point getTime(){
-  return chrono::high_resolution_clock::now();
-}
-
-int read_time(){
-  ifstream myfile ("/home/pi/wakeuptime.txt");
-  int target_time;
-  if (myfile.is_open()){
-    myfile >> target_time;
-  }else{
-    cerr << "wakeuptime.txt not found!" << endl;
-    target_time = time(0) + 25;
-  }
-  return target_time;
-}
-
-int main(){
-  try{
-    if(gpioInitialise() < 0) throw 1;
-  }
-  catch (int e) {
+int main() {
+  try {
+    if (gpioInitialise() < 0)
+      throw 1;
+  } catch (int e) {
     cout << "GPIO pins could not be initialized" << endl;
   }
 
-  
-  
   int time_until_wakeup = read_time() - time(0);
-  
+
   ADS1115_ADC adc_handler(I2C_ADDR, SHUNT_OHMS);
   ports_t ports = {4, 14, 15, 18};
   vector<float> curs(4);
@@ -64,32 +30,80 @@ int main(){
     gpioSetPWMrange(ports.at(i), steps);
     gpioPWM(ports[i], 0);
   }
+
   while (1) {
-    index ++;
-    if (index % 10000 == 0){
+    index++;
+    if (index % 10000 == 0) {
       time_until_wakeup = read_time() - time(0);
     }
-    float time = ((chrono::duration<float>)(getTime() - init)).count() - time_until_wakeup;
-    for (int light = 0; light < 4; light++){
+    float time = ((chrono::duration<float>)(getTime() - init)).count() -
+                 time_until_wakeup;
+
+    for (int light = 0; light < 4; light++) {
       float target = getTarget(light, time);
-      //gpioPWM(ports.at(light), (int)(steps * 10 * target));
-      curs[(light-1+curs.size())%curs.size()] = adc_handler.getCurrent(light);
+      // gpioPWM(ports.at(light), (int)(steps * 10 * target));
+      curs[(light - 1 + curs.size()) % curs.size()] =
+          adc_handler.getCurrent(light);
       float cur = curs[light];
-      if (target < 0.01){
-	val[light] = 0;
-      }else{
-	const float P = 0.01;
-	int delta = lround(P * (target - cur)*steps);
-	val[light] += delta;
-	const int maxdelta = steps / 1000;
-	if (val[light] > steps+maxdelta) val[light]=steps+maxdelta;
-	if (val[light] < -maxdelta) val[light]=-maxdelta;
+
+      if (target < 0.01) {
+        val[light] = 0;
+      } else {
+        const float P = 0.01;
+        int delta = lround(P * (target - cur) * steps);
+        val[light] += delta;
+        const int maxdelta = steps / 1000;
+        if (val[light] > steps + maxdelta)
+          val[light] = steps + maxdelta;
+        if (val[light] < -maxdelta)
+          val[light] = -maxdelta;
       }
-      gpioPWM( ports[light], max((short)0, min( steps,val[light])));
-      if (index % 1000 == 0){
-	cout << "Light " << light << ": " << cur << "A (" << val[light]<<"), " << adc_handler.getVoltage(light) - adc_handler.offsets_[light] << "V, Target: " << getTarget(light, time) << "A." << endl;
+
+      gpioPWM(ports[light], max((short)0, min(steps, val[light])));
+      if (index % 1000 == 0) {
+        cout << "Light " << light << ": " << cur << "A (" << val[light] << "), "
+             << adc_handler.getVoltage(light) - adc_handler.offsets_[light]
+             << "V, Target: " << getTarget(light, time) << "A." << endl;
       }
     }
   }
   return 0;
+}
+
+float getTarget(int light, float time) {
+
+  const float target_brightness = 0.3;
+  if (time > 0)
+    // It's already time for full brightness
+    return target_brightness;
+
+  float ramp_time;
+  // Use different Ramp Times for different LEDs such that we start with the warmer colors
+  if (light % 2 == 0) {
+    ramp_time = 10;
+  } else {
+    ramp_time = 20;
+  }
+
+  if (time < -ramp_time)
+    // It's not time for turning on the LEDs
+    return 0;
+
+  return target_brightness * (ramp_time + time) / ramp_time;
+}
+
+std::chrono::high_resolution_clock::time_point getTime() {
+  return chrono::high_resolution_clock::now();
+}
+
+int read_time() {
+  ifstream myfile("/home/pi/wakeuptime.txt");
+  int target_time;
+  if (myfile.is_open()) {
+    myfile >> target_time;
+  } else {
+    cerr << "wakeuptime.txt not found!" << endl;
+    target_time = time(0) + 25;
+  }
+  return target_time;
 }
--- amp_new.h
+++ amp_new.h
@@ -1,14 +1,36 @@
 #ifndef AMP_H
 #define AMP_H
 
-#include <iostream>
 #include <chrono>
-#include <pigpio.h>
 #include <ctime>
+#include <current_control.h>
+#include <iostream>
+#include <pigpio.h>
 
-template <typename T> int sign(T val) {
-  return (T(0) < val) - (val < T(0));
-}
+template <typename T> int sign(T val) { return (T(0) < val) - (val < T(0)); }
+
+/**
+ * @brief Calculates the target current for the LEDs.
+ *
+ * @param light The index of the LED considered
+ * @param time The time remaining until full brightness
+ *
+ * @return
+ */
+float getTarget(int light, float time);
+
+/**
+ * @brief Interface for a high resolution clock from chrono
+ *
+ * @return Returns a std::chrono::time_point representing the current value of the clock
+ */
+std::chrono::high_resolution_clock::time_point getTime();
+
+/**
+ * @brief
+ *
+ * @return
+ */
+int read_time();
 
 #endif
-
